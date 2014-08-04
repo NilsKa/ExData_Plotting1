4d41f61e0374db6c13237fb263e749447c089af3
NilsKa <NilsKa@users.noreply.github.com>
2014-08-04 12:29:42 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,41 @@
+###################################
+# Plot 3 - householdData
+###################################
+
+## Set working directory and read in data
+setwd("C:/Users/Administrator/Documents/R/householdData")
+
+data <- read.csv(file = "household_power_consumption.txt", 
+                 header = TRUE, sep = ";")
+
+## Subset data to relevant data and set date/time vectors to POSIXlt
+plot_data <- subset(data, Date == "1/2/2007" |
+                          Date == "2/2/2007")
+
+plot_data$DateTime <- strptime(paste(plot_data$Date, plot_data$Time), 
+                               format = "%d/%m/%Y %H:%M:%S")
+
+plot_data$Date <- NULL
+plot_data$Time <- NULL
+
+## Transform vector types of plot_data
+plot_data[plot_data == "?"] <- NA
+
+for(i in 1:6)     {
+      plot_data[, i] <- as.numeric(as.character(plot_data[[i]]))
+}
+
+## Now creating the plot
+
+# Plot 3
+png(filename = "plot3.png", width = 480, height = 480)
+
+with(plot_data, plot(DateTime, Sub_metering_1, type = "l", 
+                     ylab = "Energy sub metering", xlab = ""))
+with(plot_data, lines(DateTime, Sub_metering_2, col = "red"))
+with(plot_data, lines(DateTime, Sub_metering_3, col = "blue"))
+legend("topright", col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1,1))
+
+dev.off()
