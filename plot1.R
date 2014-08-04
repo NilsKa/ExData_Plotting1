513f931650c4c0cb6a022cb94ba4c26862a164bd
NilsKa <NilsKa@users.noreply.github.com>
2014-08-04 12:28:29 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,37 @@
+###################################
+# Plot 1 - householdData
+###################################
+
+## Set working directory and read in data
+setwd("C:/Users/Administrator/Documents/R/householdData")
+
+data <- read.csv(file = "household_power_consumption.txt", 
+                 header = TRUE, sep = ";")
+
+## Subset data to relevant data and set date/time vectors to POSIXlt
+plot_data <- subset(data, Date == "1/2/2007" |
+                          Date == "2/2/2007")
+
+plot_data$DateTime <- strptime(paste(plot_data$Date, plot_data$Time), 
+                               format = "%d/%m/%Y %H:%M:%S")
+
+plot_data$Date <- NULL
+plot_data$Time <- NULL
+
+## Transform vector types of plot_data
+plot_data[plot_data == "?"] <- NA
+
+for(i in 1:6)     {
+      plot_data[, i] <- as.numeric(as.character(plot_data[[i]]))
+}
+
+## Now creating the plot
+
+# Plot 1
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(plot_data$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+dev.off()
